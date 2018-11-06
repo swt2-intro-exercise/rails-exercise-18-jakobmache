2d97b227b828cb8b993fb316818b2c35aed8154e
Jakob Mache <jakobmache@gmail.com>
2018-11-06 15:39:03 +0100

    Added failing tests for #4

--- spec/features/author/new_author_spec.rb
+++ spec/features/author/new_author_spec.rb
@@ -14,4 +14,14 @@ describe "New author page", type: :feature do
    expect(page).to have_field('author[last_name]')
    expect(page).to have_field('author[homepage]')
  end
+
+  it 'creates an author in the database after submitting the author form' do
+    visit new_author_path
+    page.fill_in 'author[first_name]', with: 'Alan'
+    page.fill_in 'author[last_name]', with: 'Turing'
+    page.fill_in 'author[homepage]', with: 'http://wikipedia.org/Alan_Turing'
+    find('input[type="submit"]').click
+
+    expect(Author.count).to eq(1)
+  end
 end
