15e65c663300ca47aa163784b0b7456b3ac54a6c
Jakob Mache <jakobmache@gmail.com>
2018-11-07 15:32:49 +0100

    Added failing test for #11

--- spec/features/author/edit_author_spec.rb
+++ spec/features/author/edit_author_spec.rb
@@ -7,4 +7,16 @@ describe "Edit author page", type: :feature do
   it "should render withour error" do
     visit edit_author_path(author)
   end
+
+  it 'should update the author object in the database' do
+    visit edit_author_path(author)
+    page.fill_in 'author[first_name]', with: 'Alan'
+    page.fill_in 'author[last_name]', with: 'Mathison'
+    find('input[type="submit"]').click
+
+    author.reload
+
+    expect(author.first_name).to eq('Alan')
+    expect(author.last_name).to eq('Mathison')
+  end
 end
