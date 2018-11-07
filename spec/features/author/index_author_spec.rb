c2f1907cc639bdac3e922b155d0391922125c518
Jakob Mache <jakobmache@gmail.com>
2018-11-07 15:40:08 +0100

    Added failing tests for #12

--- spec/features/author/index_author_spec.rb
+++ spec/features/author/index_author_spec.rb
@@ -11,4 +11,12 @@ describe "Index author page", type: :feature do
     expect(page).to have_text('Alan Turing')
     expect(page).to have_text('http://wikipedia.de/Alan_Turing')
   end
+
+  it 'should show a link to the author edit page' do
+    @alan = FactoryBot.create :author
+
+    visit authors_path
+
+    expect(page).to have_link 'Edit', href: edit_author_path(@alan)
+  end
 end
