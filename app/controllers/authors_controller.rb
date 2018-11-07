d71ea3585f707e99c43140437418b31b108eed89
Jakob Mache <jakobmache@gmail.com>
2018-11-07 15:59:11 +0100

    Added destroy action to fix #14

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -20,6 +20,13 @@ class AuthorsController < ApplicationController
       render 'edit'
     end
   end
+  
+  def destroy
+    @author = Author.find(params[:id])
+    @author.destroy
+
+    redirect_to authors_path
+  end
 
   def create
     @author = Author.new(author_params)
