6ff201eb4d81a09577d455c8bdd6dd01cf833dfa
Jakob Mache <jakobmache@gmail.com>
2018-11-08 10:21:57 +0100

    Added failing tests for #21

--- spec/features/papers/edit_paper_spec.rb
+++ spec/features/papers/edit_paper_spec.rb
@@ -9,4 +9,18 @@ describe "Edit paper page", type: :feature do
 
     expect(page).to have_css('select[multiple]')
   end
+
+  it 'should save the new authors after selecting them' do
+    alan = FactoryBot.create :author, first_name: 'Alan', last_name: 'Turings'
+    peter = FactoryBot.create :author, first_name: 'Peter', last_name: 'Plagiarist'
+    paper.authors << alan
+    paper.save
+
+    visit edit_paper_path(paper)
+
+    find('select[multiple]').select peter.name
+    find('input[type="submit"]').click
+
+    expect(paper.authors).to include(peter)
+  end
 end
